16ad8a501a2e9dcf27defaf5243d314e775cf6ff
Jamie <naaszja@gmail.com>
2021-02-11 11:29:40 -0600

    dependencies added and installed, .env added

--- database.sql
+++ database.sql
@@ -2,11 +2,16 @@ CREATE DATABASE "giphy_search_favorites";
 
 -- You'll need a table for storing each giphy image favorite
 -- Each favorite image can be assigned 1 of the following categories as a Foreign Key
+CREATE TABLE "favorites" (
+    "id" SERIAL PRIMARY KEY,
+    "name"  VARCHAR (100) NOT NULL
+    "url" VARCHAR (100) NOT NULL
+);
 
 -- Category table
 CREATE TABLE "category" (
     "id" SERIAL PRIMARY KEY,
-    "name" VARCHAR (100) NOT NULL
+    "name" VARCHAR (100) NOT NULL 
 );
 
 -- Default categories. You may change them :)
