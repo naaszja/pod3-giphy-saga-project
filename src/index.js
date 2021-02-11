c2f767dfdd9474b8e5cd853393014fe9a5325eb5
noahwagner20 <noah.wagner1998@gmail.com>
2021-02-11 11:30:08 -0600

    search box

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import ReactGiphySearchbox from "react-giphy-searchbox";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => (
+  <>
+    <header>
+      <h1>Giphy Search</h1>
+      <p>
+        Select a gif (clicking on it)
+      </p>
+    </header>
+    <div className="searchboxWrapper">
+      <ReactGiphySearchbox
+        apiKey="Z1gSUoqXSaGAFhnFmTWCVpg9uBRoAdXk"
+        onSelect={(item) => console.log(item)}
+        masonryConfig={[
+          { columns: 2, imageWidth: 150},
+          { mq: "900px", columns: 10, imageWidth: 120, gutter: 5 }
+        ]}
+      />
+    </div>
+  </>
+);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
